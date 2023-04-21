3a9f6466707eaae8d5e197ab3f914f9574a93d27
DNLab <mkkanhb@gmail.com>
2023-04-21 10:30:25 +0900

    update

--- cent_training/resnet/batch_predict.py
+++ cent_training/resnet/batch_predict.py
@@ -18,10 +18,10 @@ def main():
          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     # load image
-    # 指向需要遍历预测的图像文件夹
+    
     imgs_root = "/data/imgs"
     assert os.path.exists(imgs_root), f"file: '{imgs_root}' dose not exist."
-    # 读取指定文件夹下所有jpg图像路径
+   
     img_path_list = [os.path.join(imgs_root, i) for i in os.listdir(imgs_root) if i.endswith(".jpg")]
 
     # read class_indict
@@ -41,7 +41,7 @@ def main():
 
     # prediction
     model.eval()
-    batch_size = 8  # 每次预测时将多少张图片打包成一个batch
+    batch_size = 8  
     with torch.no_grad():
         for ids in range(0, len(img_path_list) // batch_size):
             img_list = []
@@ -52,7 +52,7 @@ def main():
                 img_list.append(img)
 
             # batch img
-            # 将img_list列表中的所有图像打包成一个batch
+            
             batch_img = torch.stack(img_list, dim=0)
             # predict class
             output = model(batch_img.to(device)).cpu()
